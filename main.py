793d4abbe7a53aa328cdfcf3849370b308aa2c9b
Matthew Wen <mattwen2018@gmail.com>
2022-01-28 10:11:29 -0500

    Added Systemd and Wrote Script to Test it Works (#2)

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+# https://github.com/pika/pika
+import os
 from time import sleep
+import datetime
+import pika
 
-while True:
-    print("Hello World")
-    sleep(3)
+
+def main() -> None:
+    count: int = 0
+    while True:
+        x = datetime.datetime.now()
+        print(f'Hello World: {x}')
+        os.system(f'echo {count} >> logs.txt')
+        count += 1
+        sleep(3600)
+
+
+if __name__ == "__main__":
+    main()
