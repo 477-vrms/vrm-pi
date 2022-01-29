98d280663c02e002a3ba23c9757634b0d02fb9c9
Matthew Wen <mattwen2018@gmail.com>
2022-01-29 01:41:21 -0500

    Can Send Messages to the Server

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 # https://github.com/pika/pika
 import os
+import socket
 from time import sleep
 import datetime
+from multiprocessing import Process
 import pika
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-def main() -> None:
+
+def server() -> None:
+    count: int = 0
+    while count < 10:
+        print("sending message")
+        s.sendto(b"What Up Matt Wen", ("localhost", 8002))
+        sleep(1)
+        count += 1
+
+
+def example() -> None:
     count: int = 0
     while True:
         x = datetime.datetime.now()
@@ -13,6 +28,22 @@ def main() -> None:
         os.system(f'echo {count} >> logs.txt')
         count += 1
         sleep(3600)
+
+
+def client() -> None:
+    print("listening for messages")
+    while True:
+        data, addr = s.recvfrom(1024)
+        print("received message: %s" % data)
+
+
+def main() -> None:
+    p1 = Process(target=client)
+    p2 = Process(target=server)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
 
 
 if __name__ == "__main__":
