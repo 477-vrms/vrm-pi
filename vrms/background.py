556ffdb64623f3e6f342b67f36a31c49fd2a8982
477-vrms-pi <mattwen2022@outlook.com>
2022-02-09 17:26:01 +0000

    Added Serial to Pi

--- vrms/background.py
+++ vrms/background.py
@@ -1,7 +1,9 @@
 from multiprocessing import Process
 import os
+import serial
 from time import sleep
 import datetime
+import wiringpi
 
 from vrms.hardware.arm import ArmHandler
 from vrms.network.mqtt import Mqtt
@@ -32,13 +34,20 @@ def udp() -> None:
 def arm() -> None:
     a.client()
 
+def uart_tx_rx() -> None: 
+    wiringpi.wiringPiSetup()
+    serial = wiringpi.serialOpen('/dev/ttyAMA0',9600)
+    print(serial)
+    while True:
+        print(wiringpi.serialDataAvail(serial))
 
 class Background:
 
     def __init__(self):
         self.p1 = Process(target=example)
         self.p2 = Process(target=mqtt)
-        self.p3 = Process(target=udp)
+        #self.p3 = Process(target=udp)
+        self.p3 = Process(target=uart_tx_rx)
 
     def listen(self):
         self.p1.start()
