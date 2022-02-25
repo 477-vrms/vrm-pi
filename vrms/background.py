4f9ad216b9c4b2fa182ba1cd6be6f61358e1a382
Pi <mattwen2022@outlook.com>
2022-02-25 18:06:39 +0000

    Emma Changes on PI

--- vrms/background.py
+++ vrms/background.py
@@ -4,6 +4,7 @@ from time import sleep
 import datetime
 import serial
 
+
 from vrms.hardware.arm import ArmHandler
 from vrms.network.mqtt import Mqtt
 from vrms.network.udp import Udp
@@ -34,16 +35,21 @@ def arm() -> None:
     pass
     # a.client()
 
+data = '11110000'
 
 def uart_tx_rx() -> None:
     try:
-        ser = serial.Serial("/dev/ttyS0", 9600)  # Open port with baud rate
+        ser = serial.Serial("/dev/ttyAMA0", 115200)  # Open port with baud rate
         while True:
-            received_data = ser.read()  # read serial port
+            print(ser.get_settings())
+            ser.write(data.encode('utf-8'))  # transmit data serially
             sleep(0.03)
+            ser.flush()
+            print(ser.out_waiting)
+            print(ser.in_waiting)
             data_left = ser.inWaiting()  # check for remaining byte
+            received_data = ser.read()  # read serial port
             received_data += ser.read(data_left)
-            ser.write(received_data)  # transmit data serially
     except Exception as e:
         print(e)
 
@@ -53,17 +59,17 @@ class Background:
     def __init__(self):
         self.p1 = Process(target=example)
         self.p2 = Process(target=mqtt)
-        self.p3 = Process(target=udp)
-        self.p4 = Process(target=uart_tx_rx)
+        #self.p3 = Process(target=udp)
+        self.p3 = Process(target=uart_tx_rx)
 
     def listen(self):
         self.p1.start()
         self.p2.start()
         self.p3.start()
-        self.p4.start()
+        #self.p4.start()
 
     def close(self):
         self.p1.join()
         self.p2.join()
         self.p3.join()
-        self.p4.join()
+        #self.p4.join()
