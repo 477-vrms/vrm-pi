bc5ea2dce9717a656e942da3a639d6042a0fef36
Emma Clary <eclary2000@gmail.com>
2022-03-02 05:51:57 -1000

    Pi Changes

--- vrms/background.py
+++ vrms/background.py
@@ -2,7 +2,6 @@ from multiprocessing import Process
 import os
 from time import sleep
 import datetime
-import serial
 
 from vrms.hardware.arm import ArmHandler
 from vrms.network.mqtt import Mqtt
@@ -29,38 +28,19 @@ def udp() -> None:
     u = Udp()
     u.client()
 
-
-def uart_tx_rx() -> None:
-    test_string = "Test serial port ...".encode('utf-8')
-    port_list = ["/dev/ttyAMA1"]#,"/dev/ttyAMA1","/dev/ttyS0","/dev/ttyS"]
-    while 1:
-        try:
-            serialPort = serial.Serial("/dev/ttyAMA1", 9600, timeout = 2)
-            #print ("Serial port", "/dev/ttyAMA1", " ready for test :")
-            bytes_sent = serialPort.write(test_string)
-            #print ("Sended", bytes_sent, "byte")
-            loopback = serialPort.read(bytes_sent)
-            #print("Received ",len(loopback), "bytes. Port", "/dev/ttyAMA1")
-            serialPort.close()
-        except IOError:
-            print("Error on", "/dev/ttyAMA1","\n")
-
 class Background:
 
     def __init__(self):
         self.p1 = Process(target=example)
         self.p2 = Process(target=mqtt)
         self.p3 = Process(target=udp)
-        self.p4 = Process(target=uart_tx_rx)
 
     def listen(self):
         self.p1.start()
         self.p2.start()
         self.p3.start()
-        self.p4.start()
 
     def close(self):
         self.p1.join()
         self.p2.join()
         self.p3.join()
-        self.p4.join()
--- vrms/hardware/arm.py
+++ vrms/hardware/arm.py
@@ -1,11 +1,41 @@
-def on_retrieve_position(arm_json) -> None:
+import serial
+
+def uart_tx_rx(self) -> None:
+    j_str = (str(self.j1)+":"+str(self.j2)+":"+str(self.j3)+":"+str(self.j4)+":"+str(self.j5)+":"+str(self.j6)+":"+str(self.j7)+":"+str(self.j8)+"$").encode('utf-8')
+    print(j_str)
+    serialPort = serial.Serial("/dev/ttyAMA1", 9600, timeout = 2)
+    bytes_sent = serialPort.write(j_str)
+    print("SENT: ")
+    print(bytes_sent)
+    loopback = serialPort.read(bytes_sent)
+    print("Received")
+    print(loopback)
+
+def on_retrieve_position(self,arm_json) -> None:
     print(arm_json)
+    self.j1 = arm_json["J1"]
+    self.j2 = arm_json["J2"]
+    self.j3 = arm_json["J3"]
+    self.j4 = arm_json["J4"]
+    self.j5 = arm_json["J5"]
+    self.j6 = arm_json["J6"]
+    self.j7 = arm_json["J7"]
+    self.j8 = arm_json["J8"]
 
 
 class ArmHandler:
 
     def __init__(self):
+        self.j1 = 1500
+        self.j2 = 1500
+        self.j3 = 1500
+        self.j4 = 1500
+        self.j5 = 1500
+        self.j6 = 1500
+        self.j7 = 1500
+        self.j8 = 1500
         pass
 
     def add_json(self, item):
-        on_retrieve_position(arm_json=item)
+        on_retrieve_position(self,arm_json=item)
+        uart_tx_rx(self)
