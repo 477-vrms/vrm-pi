7d2d7553acd2a53636561de569a3c7b74bfb747e
Matthew Wen <mattwen2018@gmail.com>
2022-04-03 16:22:18 -0400

    Added Ready and Start

--- vrms/network/mqtt.py
+++ vrms/network/mqtt.py
@@ -3,6 +3,7 @@ from zmq import Socket
 import json
 
 from vrms.hardware.arm import ArmHandler
+from vrms.network.udp import Udp
 
 
 class Mqtt:
@@ -17,10 +18,25 @@ class Mqtt:
 
     def __init__(self):
         self.ctx = zmq.Context()
+
         self.subscriber: Socket = self.ctx.socket(zmq.SUB)
         self.subscriber.connect("tcp://34.132.95.250:2001")
         self.subscriber.setsockopt(zmq.SUBSCRIBE, b"vrms_pi")
+
         self.arm_handler = ArmHandler.load_arm()
+
+    def handle_obj(self, obj):
+        if obj["action"] == "move":
+            self.arm_handler.enqueue(obj)
+        elif obj["action"] == "video_ready":
+            udp = Udp.load_udp()
+            udp.start_sending()
+        elif obj["action"] == "video_start":
+            udp = Udp.load_udp()
+            udp.set_is_sent(True)
+        elif obj["action"] == "video_end":
+            udp = Udp.load_udp()
+            udp.stop_sending()
 
     def client(self, lock) -> None:
         while True:
@@ -28,11 +44,10 @@ class Mqtt:
             if response != b"vrms_pi":
                 try:
                     str_json = response.decode("UTF-8")
-                    #print(str_json)
                     obj = json.loads(str_json)
                     lock.acquire()
                     try:
-                        self.arm_handler.enqueue(obj)
+                        self.handle_obj(obj)
                     finally:
                         lock.release()
                 except json.JSONDecodeError as e:
--- vrms/network/udp.py
+++ vrms/network/udp.py
@@ -1,5 +1,6 @@
 import json
 import socket
+from time import sleep
 
 
 class Udp:
@@ -19,16 +20,34 @@ class Udp:
         self.c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.c.bind(("127.0.0.1", 4200))
 
+        self.is_sending = False
+
     def send_response(self, response):
         self.s.sendto(response, ("34.132.95.250", 2002))
 
-    def client(self, lock) -> None:
+    def start_sending(self):
         init = {
             "id": "vrms-pi",
             "password": "FAKEPASSWORD"
         }
         response = json.dumps(init).encode('utf-8')
-        self.send_response(response)
+        while not self.is_sending:
+            self.send_response(response)
+            sleep(1)
+
+    def set_is_sent(self, is_sent):
+        self.is_sending = is_sent
+
+    def send_frame(self):
+        frame = self.c.recvfrom(1024 * 65)
+        self.send_response(frame[0])
+
+    def stop_sending(self):
+        self.is_sending = False
+
+    def client(self, lock) -> None:
         while True:
-            frame = self.c.recvfrom(1024 * 65)
-            self.send_response(frame[0])
+            if self.is_sending:
+                self.send_frame()
+            else:
+                sleep(1)
